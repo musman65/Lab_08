030062a70210dc083fa4dd788702460582433501
Usman Muhammad <68919810+musman65@users.noreply.github.com>
2025-11-03 11:23:38 -0400

    Updated Main.java

--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -1,3 +1,11 @@
+//https://github.com/musman65/Lab_08
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -7,13 +15,22 @@
  *
  * @author 6298674
  */
-public class Main {
+public class Main extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Application.launch(Main.class, args);
+    }
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("file.fxml"));
+        
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
     
 }
